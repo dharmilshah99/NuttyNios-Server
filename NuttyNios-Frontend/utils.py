ab06148d6a3a9af41495878979610055fd1cfe74
dharmilshah99 <dharmil.shah@hotmail.com>
2022-02-19 18:47:02 +0000

    Fix utils

--- NuttyNios-Frontend/utils.py
+++ NuttyNios-Frontend/utils.py
@@ -2,11 +2,12 @@ import time
 import paho.mqtt.client as paho
 
 class MQTT(object):
-    def __init__(self, client, hostname, port, topic):
+    def __init__(self, client, hostname, port, topic, ca_cert, certfile, keyfile):
         """Initialize class."""
         self.client = client
         self.topic = topic
         self.hostname, self.port = hostname, port
+        self.ca_cert, self.certfile, self.keyfile = ca_cert, certfile, keyfile
 
     def on_connect(self, client, userdata, flags, rc):
         """Subscribe to topic."""
@@ -23,7 +24,7 @@ class MQTT(object):
         while not is_connected:
             try:
                 self.client.username_pw_set(username="", password="")
-                self.client.tls_set(ca_certs="", certfile="", keyfile="")
+                self.client.tls_set(ca_certs=self.ca_cert, certfile=self.certfile, keyfile=self.keyfile)
                 self.client.on_connect = self.on_connect
                 self.client.on_message = self.on_message
                 self.client.connect(self.hostname, self.port)
