5494c3d7cfef129ac7eaca89c74f3b0aec8f7d75
dharmilshah99 <dharmil.shah@hotmail.com>
2022-02-20 10:14:26 +0000

    Add MQTT bridge

--- utils/generate_passwords.sh
+++ utils/generate_passwords.sh
@@ -4,6 +4,12 @@ DEFAULT_DIR="../mosquitto/config"
 PASSWORD_FILE_PATH=${1}
 DIR=${2:-$DEFAULT_DIR}
 
+# Check passwords file name
+if [ ! -f "$(dirname PASSWORD_FILE_PATH)/passwords.txt" ]; then
+    echo "Incorrect file name!"
+    exit 1
+fi
+
 # Generate
 mosquitto_passwd -U $PASSWORD_FILE_PATH
 mv $PASSWORD_FILE_PATH $DEFAULT_DIR
